10f58853a7b54eb717f8cc60907ce3b8746f9a6a
rokko <rocco.caricola89@gmail.com>
2022-01-25 19:05:06 +0100

    fix api

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res){
 
 
 //GET TOKEN METADATA FROM TOKENID
-app.get('/api/asset/:token_id\.:ext?', function(req, res) {
+app.get('/asset/:token_id\.:ext?', function(req, res) {
     const tokenId = parseInt(req.params.token_id).toString();
     const tokenUri = ct.getTokenUri(tokenId);
     res.send(tokenUri);
@@ -29,7 +29,7 @@ app.get('/api/asset/:token_id\.:ext?', function(req, res) {
 
 
 //GENERATE METADATA FROM TOKENID AND INSERT INTO DB
-app.post('/api/asset/:data', async function(req, res) {
+app.post('/asset/:data', async function(req, res) {
     const data = req.params.data;
     const content = await ct.generateMetadata(JSON.parse(data));
     
@@ -44,7 +44,7 @@ app.post('/api/asset/:data', async function(req, res) {
 
 
 //UPDATE METADATA ATTRIBUTE FROM TOKENID
-app.post('/api/asset/change/:data', async function(req, res) {
+app.post('/asset/change/:data', async function(req, res) {
     const data = req.params.data;
     const json_data = JSON.parse(data);
     console.log(json_data['attributes']);
@@ -54,69 +54,69 @@ app.post('/api/asset/change/:data', async function(req, res) {
 
 
 //RETRIEVE ATTRIBUTES OF AN ITEM
-app.get('/api/asset/attributes/:name', async function(req, res) {
+app.get('/asset/attributes/:name', async function(req, res) {
     const atts = await md.getAttributes(req.params.name);
     return res.send(atts);
 })
 
 
 //RETRIEVE PALETTE COLOR
-app.get('/api/colors', async function(req, res) {
+app.get('/colors', async function(req, res) {
     const colors = await md.getColors();
     return res.send(colors);
 })
 
 //RETIREVE RARITIES
-app.get('/api/rarities', async function(req, res) {
+app.get('/rarities', async function(req, res) {
     const rarities = await md.getRarities();
     return res.send(rarities);
 })
 
 
 //RETRIEVE BRANDS
-app.get('/api/brands', async function(req, res) {
+app.get('/brands', async function(req, res) {
     const brands = await md.getBrands();
     return res.send(brands);
 })
 
 
 //RETRIEVE TIPOLOGIES
-app.get('/api/tipologies', async function(req, res) {
+app.get('/tipologies', async function(req, res) {
     const tipologies = await md.getTipologies();
     return res.send(tipologies);
 })
 
 
 //RETRIEVE ZONE
-app.get('/api/zones', async function(req, res) {
+app.get('/zones', async function(req, res) {
     const zones = await md.getZones();
     return res.send(zones);
 })
 
 
 //RETRIEVE LAND_TIPOLOGY
-app.get('/api/lands', async function(req, res) {
+app.get('/lands', async function(req, res) {
     const lands = await md.getLands();
     return res.send(lands);
 })
 
 
 //RETRIEVE STRUCTURE
-app.get('/api/lands_parcel', async function(req, res) {
+app.get('/lands_parcel', async function(req, res) {
     const parcels = await md.getLandParcel();
     return res.send(parcels);
 })
 
 
 //RETRIEVE APARTMENT_SIZE
-app.get('/api/apartment_size', async function(req, res) {
+app.get('/apartment_size', async function(req, res) {
     const ap_size = await md.getApartSize();
     return res.send(ap_size);
 })
 
 
 //RETRIEVE IMAGE FROM TOKENID
-app.get('/api/image/:token_id\.:ext?', async function(req, res) {
+app.get('/image/:token_id\.:ext?', async function(req, res) {
     const token_id = parseInt(req.params.token_id).toString();
     if(ct.existTokenId(token_id)){
         return res.sendFile(__dirname + "/nft/"+token_id+".mp4");
